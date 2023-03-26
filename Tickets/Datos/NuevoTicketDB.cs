452a63cdcc8fc52fed647a2595307391053cdf25
mleolopez7 <mleolopez7@gmail.com>
2023-03-25 20:36:34 -0600

    parte 3

---
+++ Tickets/Datos/NuevoTicketDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto.Tls;
+
+namespace Datos
+{
+    internal class NuevoTicketDB
+    {
+        string cadena = "server=localhost; user=root; database=ticketdb password=root2023;";
+
+
+    }
+}
